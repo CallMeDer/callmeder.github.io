9fba16875ab52103521a74057bbbfcc24bda7772
CallMeDer <95129811+CallMeDer@users.noreply.github.com>
2022-07-13 16:10:06 +0700

    Add new project code

---
+++ fb/js/band.js
@@ -0,0 +1,10 @@
+const buyBtns = document.querySelectorAll('.js-buy-ticket')
+        const modal = document.querySelector('.modal')
+
+        function showBuyTicket() {
+            modal.classList.add('open')
+        }
+
+        for (const buyBtn of buyBtns) {
+             buyBtn.addEventListener('click', showBuyTicket)
+        }
